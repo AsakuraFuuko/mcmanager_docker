ece71ea120f93e1b4189d6f4d762047c17b4caf0
Fuuko <3427282+AsakuraFuuko@users.noreply.github.com>
2020-05-24 12:01:26 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,12 @@ if [ ! -f /home/MCSManager/property.js ]; then
     cd /home 
     rm -rf MCSManager
     git clone https://github.com/Suwings/MCSManager.git
+    chown $PUID:$PGID -r MCSManager
     cd MCSManager 
     npm install --production
     node app.js
 else
+    chown $PUID:$PGID -r MCSManager
     cd /home/MCSManager
     git pull --rebase
     npm install --production
